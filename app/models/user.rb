b56b6835143eb7a80f203b5c0baa8947136afa10
Walter <dalewaltergh@gmail.com>
2022-04-02 02:51:02 +0800

    reinstall rails_admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,7 @@ class User < ApplicationRecord
          :recoverable,
          :rememberable,
          :validatable,
-         :confirmable,
-         :trackable
+         :confirmable
 
   # enum role: [:user, :admin]
   # after_initialize :set_defaut_role, :if => :new_record?
